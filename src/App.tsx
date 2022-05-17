ebfcca15c8807b66c0ef759d16cefd7912889968
fahad0samara <fah8ad@outlook.com>
2022-05-17 19:46:14 +0300

    add first bg

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+
+import { useContext } from 'react'
+import { themeContext } from './Context'
+import Navbar from './Navbar';
+import  {Section}  from "./Home/Section"
+function App() {
+  const theme = useContext(themeContext)
+  const darkMode = theme.state.darkMode
+
+  return (
+    <div
+      className=""
+      style={{
+        background: darkMode ? 'black' : '',
+        color: darkMode ? 'white' : '',
+      }}
+
+    >
+
+
+      <Navbar/>
+    <Section/>
+    </div>
+  )
+}
+
+export default App
